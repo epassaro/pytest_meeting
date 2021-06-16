01931d9f6f708177895993d31de3ac103ed0abe4
Ezequiel Pássaro <epassaro15@gmail.com>
2021-06-16 11:15:34 -0300

    simplify tests (remove fixtures)

--- test_example.py
+++ test_example.py
@@ -3,34 +3,21 @@ import pytest
 from example import Complex
 
 """
-You can't just define `z` and `w`:
-
-z = Complex(2, 2)
-w = Complex(1, 2)
-
-... fixtures are needed!
-"""
-
-@pytest.fixture
-def z():
-    return Complex(4,3)
-
-@pytest.fixture
-def w():
-    return Complex(1,2)
-
-"""
 Then write your tests. They're just a bunch
 of functions and assertions.
 """
 
-def test_repr(z, w):
+def test_repr():
+    z = Complex(4, 3)
     assert z.__repr__() == "4 + 3i"
 
-def test_add(z, w):
+def test_add():
+    z = Complex(4, 3)
+    w = Complex(1, 2)
     v = z + w
     assert str(v) == "5 + 5i"
 
 @pytest.mark.skip  # Mark the test to be skipped by `pytest`
-def test_mod(z):
+def test_mod():
+    z = Complex(4, 3)
     z.mod() == 5
